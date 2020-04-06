b26ccf0497a632459e6a856ecf4b386fb2774cfd
fuzzychi <61358920+fuzzychi@users.noreply.github.com>
2020-04-06 09:50:25 -0400

    more changes

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import './App.css';
 import  { db } from './firebase';
 
 function App() {
 
   const [names, setNames] = useState([]);
+    useEffect(()=>{
     const collection = db.collection("names");
     collection.onSnapshot(snapshot =>{
       const newNames = [];
       snapshot.forEach(doc =>{
-        const item = {
-          id : doc.id,
+        const item ={
+          id: doc.id,
           firstname: doc.data().firstname,
           lastname: doc.data().lastname
         }
@@ -18,11 +19,21 @@ function App() {
       })
       setNames(newNames);
     })
+  },[]);
   
+
   const handleChange = event =>
   {
-      const id = event.target.id;
-      console.log(id);
+      const newNames = names;
+      const item = names.find(element => element.id === event.target.id);
+      item.firstname = event.target.value;
+      const doc = db.collection("names").doc(event.target.id)
+      doc.update({
+        firstname: event.target.value,
+      }).then(()=>{
+        console.log("Updated");
+      });
+      setNames([...newNames])
   }
   return (
     <div className="App">
