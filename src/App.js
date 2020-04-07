f0a67f69ed62b74dc2d26288ae28146e24ec7b88
fuzzychi <dominance@gmail.com>
2020-04-07 18:56:34 -0400

    new changes

--- src/App.js
+++ src/App.js
@@ -3,6 +3,12 @@ import './App.css';
 import  { db } from './firebase';
 import { makeStyles } from '@material-ui/core/styles';
 import TextField from '@material-ui/core/TextField';
+import Typography from '@material-ui/core/Typography';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import Button from '@material-ui/core/Button';
+import { CardHeader } from '@material-ui/core';
 
 
 function App() {
@@ -11,21 +17,25 @@ function App() {
     root: {
       '& > *': {
         margin: theme.spacing(1),
-        width: '25ch',
+        width: '200px',
+        padding: '2px',
       },
     },
   }));
 
+  const classes = useStyles();
+
   const [names, setNames] = useState([]);
     useEffect(()=>{
     const collection = db.collection("names");
-    collection.onSnapshot(snapshot =>{
+    collection.orderBy('date','desc').onSnapshot(snapshot =>{
       const newNames = [];
       snapshot.forEach(doc =>{
         const item ={
           id: doc.id,
           firstname: doc.data().firstname,
-          lastname: doc.data().lastname
+          lastname: doc.data().lastname,
+          date: doc.data().date
         }
         newNames.push(item);
       })
@@ -38,25 +48,51 @@ function App() {
       const newNames = names;
       const item = names.find(element => element.id === event.target.id);
       item.firstname = event.target.value;
+      setNames([...newNames])
       const doc = db.collection("names").doc(event.target.id)
       doc.update({
         firstname: event.target.value,
       }).then(()=>{
         console.log("Updated");
       });
-      setNames([...newNames])
+      
   }
+
+  const handleAdd = event =>
+  {
+    const item = {
+      firstname: "",
+      lastname: "",
+      date: new Date()
+    }
+    db.collection("names").add(item).then(console.log("added"));
+  }
+
+  const handleDelete = (props) =>
+  {
+     db.collection("names").doc(props).delete()
+  }
+
   return (
     <div className="App">
-     
+      <Typography variant="h1">Form</Typography>
+      <Button variant="contained" onClick={handleAdd}>Add</Button>
+     <form className={classes.root} noValidate autoComplete="off">
     {
       names.map(name =>
         {
         return (
-        <div><TextField label={name.id} id={name.id} value={name.firstname} onChange={handleChange} /></div>
+        <Card variant="outlined">
+        <CardHeader title="Card"/>
+        <CardContent>
+            <TextField label="Name" id={name.id} value={name.firstname} onChange={handleChange} />
+            <Button onClick={()=>{handleDelete(name.id)}}>Remove</Button>
+        </CardContent>
+        </Card>
         )
         })
     }
+    </form>
     </div>  
   );
 }
