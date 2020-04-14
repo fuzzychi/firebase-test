0f3877f360c46e6b3c2c7eff640cd561a0ea716d
fuzzychi <61358920+fuzzychi@users.noreply.github.com>
2020-04-13 21:13:09 -0400

    more changes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
-import  { db } from './firebase';
+import { db } from './firebase';
 import { makeStyles } from '@material-ui/core/styles';
 import TextField from '@material-ui/core/TextField';
 import Typography from '@material-ui/core/Typography';
@@ -11,20 +11,24 @@ import Button from '@material-ui/core/Button';
 import { CardHeader } from '@material-ui/core';
 import Grid from '@material-ui/core/Grid';
 import Paper from '@material-ui/core/Paper';
-
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import Divider from '@material-ui/core/Divider';
+import {Fragment} from 'react';
 
 function App() {
 
   const useStyles = makeStyles((theme) => ({
     root: {
-      padding: 10,
+      padding: 5,
       flexGrow: 1,
     },
     paper: {
-      height: 200,
-      width: 200,
-      marginTop: 15,
-      backgroundColor:'#eee'
+      margin: 10,
+      padding: 10,
+      backgroundColor: '#eee'
     },
     control: {
       padding: theme.spacing(2),
@@ -34,12 +38,12 @@ function App() {
   const classes = useStyles();
 
   const [names, setNames] = useState([]);
-    useEffect(()=>{
+  useEffect(() => {
     const collection = db.collection("names");
-    collection.orderBy('date','desc').onSnapshot(snapshot =>{
+    collection.orderBy('date', 'desc').onSnapshot(snapshot => {
       const newNames = [];
-      snapshot.forEach(doc =>{
-        const item ={
+      snapshot.forEach(doc => {
+        const item = {
           id: doc.id,
           firstname: doc.data().firstname,
           lastname: doc.data().lastname,
@@ -49,25 +53,23 @@ function App() {
       })
       setNames(newNames);
     })
-  },[]);
-  
-  const handleChange = event =>
-  {
-      const newNames = names;
-      const item = names.find(element => element.id === event.target.id);
-      item.firstname = event.target.value;
-      setNames([...newNames])
-      const doc = db.collection("names").doc(event.target.id)
-      doc.update({
-        firstname: event.target.value,
-      }).then(()=>{
-        console.log("Updated");
-      });
-      
+  }, []);
+
+  const handleChange = event => {
+    const newNames = names;
+    const item = names.find(element => element.id === event.target.id);
+    item.firstname = event.target.value;
+    setNames([...newNames])
+    const doc = db.collection("names").doc(event.target.id)
+    doc.update({
+      firstname: event.target.value,
+    }).then(() => {
+      console.log("Updated");
+    });
+
   }
 
-  const handleAdd = event =>
-  {
+  const handleAdd = event => {
     const item = {
       firstname: "",
       lastname: "",
@@ -76,31 +78,43 @@ function App() {
     db.collection("names").add(item).then(console.log("added"));
   }
 
-  const handleDelete = (props) =>
-  {
-     db.collection("names").doc(props).delete()
+  const handleDelete = (props) => {
+    db.collection("names").doc(props).delete()
   }
 
   return (
     <div className={classes.root}>
-      <Typography variant="h1">Form</Typography>
-      <div>
-      <Button variant="contained" onClick={handleAdd}>Add</Button>
-      </div>
-      <Grid container xs={12} spacing={2}>
-    {
-      names.map(name =>
-        {
-        return (
-        <Grid xs={3}>  
-        <Paper className={classes.paper}>
-            <TextField label="Name" id={name.id} value={name.firstname} onChange={handleChange} />
-            <Button onClick={()=>{handleDelete(name.id)}}>Remove</Button>
-        </Paper>
+      <Typography variant="h1">Configuration</Typography>
+      <Typography variant="h3">Part 1</Typography>
+      <Grid container spacing={2}>
+        <Grid item xs={2}>
+        <Paper>  
+          <List component="nav" aria-label="main mailbox folders">
+            <ListItem button>
+              <ListItemText primary="Part 1" />
+            </ListItem>
+            <ListItem button>
+              <ListItemText primary="Part 2" />
+            </ListItem>
+          </List>
+          </Paper>
         </Grid>
-        )
-        })
-    }
+        <Grid item xs={8}>
+          {
+            names.map(name => {
+              return (
+                    <Paper style={{padding:10}}>            
+                    <TextField label="Name" id={name.id} value={name.firstname} onChange={handleChange} />
+                    <Button onClick={() => { handleDelete(name.id) }} style={{ display: 'block', padding: 10, marginTop: 10 }}>Remove</Button>
+                    </Paper>
+                
+              )
+            })
+          }
+        </Grid>
+        <Grid item xs={2}>
+        <Button variant="contained" onClick={handleAdd}>Add</Button>  
+        </Grid>
     </Grid>
     </div>  
   );
